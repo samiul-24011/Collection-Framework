54f6a3d0a6f1de90803b17de28b590100c3e07e0
samiul-24011 <it24011@mbstu.ac.bd>
2025-12-25 19:07:39 +0600

    Create EmployeeHashMap.java

---
+++ EmployeeHashMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class EmployeeHashMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> emp = new HashMap<>();
+        emp.put(1, "HR");
+        emp.put(2, "IT");
+        emp.put(3, "Finance");
+        System.out.println(emp);
+    }
+}
