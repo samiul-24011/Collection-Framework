211e032e8926ae00a73bcaf0d2edfde9b5c1a68a
samiul-24011 <it24011@mbstu.ac.bd>
2025-12-25 19:13:40 +0600

    Create StudentTreeMap.java

---
+++ StudentTreeMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class StudentTreeMap {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> students = new TreeMap<>();
+        students.put(101, "Imran");
+        students.put(103, "Rahim");
+        students.put(102, "Karim");
+        System.out.println(students);
+    }
+}
